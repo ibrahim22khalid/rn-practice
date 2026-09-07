1e5a552df11d0d586abff6703944956c2a3cf522
Ibrahim Khaled <ibrahim22khaled@ppath.app>
2026-09-07 17:48:12 +0300

    update: restructure the app hierarchy

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
-import FlexboxPractice from "./components/FlexBoxPractice";
-import { ThemeProvider } from "./core/theme/ThemeContext";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import CardsScreen from "./screens/CardsScreen"; 
-import FixedLayoutScreen from "./screens/FixedLayoutScreen";
+import { ThemeProvider } from "./src/core/theme/ThemeContext";
+import CardsScreen from "./src/screens/CardsScreen";
 
 export default function App() {
   return (
     <SafeAreaProvider >
       <ThemeProvider>
-    
       <CardsScreen />
     </ThemeProvider>
     </SafeAreaProvider>
