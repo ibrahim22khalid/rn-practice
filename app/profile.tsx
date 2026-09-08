d18f5619717bd3765f8c5b27844cd71bdc54ddf7
Ibrahim Khaled <ibrahim22khaled@ppath.app>
2026-09-08 14:24:42 +0300

    add: new screens & _layout, index => to apply expo routing via link ref

---
+++ app/profile.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class profile extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>profile</Text>
+      </View>
+    )
+  }
+}
